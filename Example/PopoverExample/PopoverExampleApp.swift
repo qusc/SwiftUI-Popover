71453290ef9c1ebaf17ee577e0cdc6268337e826
Quirin Schweigert <quirin.schweigert@tum.de>
2025-11-21 12:23:12 +0100

    Initialize package with example project

---
+++ Example/PopoverExample/PopoverExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopoverExampleApp.swift
+//  PopoverExample
+//
+//  Created by Quirin Schweigert on 21.11.25.
+//
+
+import SwiftUI
+
+@main
+struct PopoverExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
